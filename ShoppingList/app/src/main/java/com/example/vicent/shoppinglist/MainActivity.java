051ed5e93b669bb959918948ec1b98611155e7d8
vbcurtis <vcurtis88@hotmail.com>
2017-02-22 17:32:36 -0500

    Shopping List

---
+++ ShoppingList/app/src/main/java/com/example/vicent/shoppinglist/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.vicent.shoppinglist;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.ListView;
+
+        import java.lang.reflect.Array;
+
+public class MainActivity extends AppCompatActivity {
+    int quantity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick()
+    {
+
+    }
+}
